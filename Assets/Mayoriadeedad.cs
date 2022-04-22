ef5750f469088edaec1418e277d544637a3352ee
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-04-22 15:37:39 -0300

    Initial commit

---
+++ Assets/Mayoriadeedad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mayoriadeedad : MonoBehaviour
+{
+
+    public int edadUsuario;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
